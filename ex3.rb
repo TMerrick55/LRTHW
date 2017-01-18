29e085b37a6e96e08f8a8f305870b823369f30c3
TMerrick55 <tmerrick44@gmail.com>
2017-01-18 17:00:33 -0500

    Add files via upload

---
+++ ex3.rb
@@ -0,0 +1,25 @@
+puts "I will now count my chickens:" # Informs us of what the numbers are in reference to.
+
+puts "Hens #{25.0 + 30.0 / 6.0}" # Lists the total number of Hens.
+puts "Roosters #{100.0 - 25.0 * 0.75}" # Lists the total number of Roosters.
+
+puts "Now I will count the eggs:" # Informs us of the change in what the numbers refer to.
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 0.50 - 1.0 / 4.0 + 6.0 # The number of eggs.
+
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?" # Puts forward an equation.
+
+puts 3.0 + 2.0 < 5.0 - 7.0 # Answers the above equation.
+
+puts "What is 3 + 2? #{3.0 + 2.0}" # Answers the left side of the equation.
+puts "What is 5 - 7? #{5.0 - 7.0}" # Answers the right side of the equation.
+
+puts "Oh, that's why it's false." # Understanding of why the above equation is false.
+
+puts "How about some more." # Proposes more mathematical problems.
+
+puts "Is it greater? #{5.0 > -2.0}" # Asks if 5 greater than -2.
+puts "Is it greater or equal? #{5.0 >= -2.0}" # Asks if 5 is greater than or equal to -2.
+puts "Is it less or equal? #{5.0 <= -2.0}" # Asks if 5 is less than or equal.
+
+# In coding, floating numbers cause margin of errors when dealing with mathematical equations.
